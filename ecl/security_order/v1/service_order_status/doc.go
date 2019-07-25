e41d07be965702a8f7ea6ac4823b8bf089587509
Keiichi <Hikita>
2019-07-25 16:54:42 +0900

    fix comment blocks.

--- ecl/security_order/v1/service_order_status/doc.go
+++ ecl/security_order/v1/service_order_status/doc.go
@@ -1,2 +1,2 @@
-// Package order_progress contains user management functionality on SSS
+// Package service_order_status contains service order management functionality on security
 package service_order_status
