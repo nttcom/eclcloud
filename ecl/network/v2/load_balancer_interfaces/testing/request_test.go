2c25bcc11b3d2880e464855457a125e31b6c0109
yapoo-t <59546929+yapoo-t@users.noreply.github.com>
2020-01-31 09:26:57 +0900

    Add Load Balancer service functionalities. (#20)
    - Add Load Balancer service functionalities.
    - Add unit test of the above.

---
+++ ecl/network/v2/load_balancer_interfaces/testing/request_test.go
@@ -0,0 +1,197 @@
+package testing
+
+import (
+	"fmt"
+	load_balancer_interfaces "github.com/nttcom/eclcloud/ecl/network/v2/load_balancer_interfaces"
+	"net/http"
+	"testing"
+
+	fake "github.com/nttcom/eclcloud/ecl/network/v2/common"
+	"github.com/nttcom/eclcloud/pagination"
+	th "github.com/nttcom/eclcloud/testhelper"
+)
+
+func TestListLoadBalancerInterface(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/load_balancer_interfaces", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, ListResponse)
+	})
+
+	client := fake.ServiceClient()
+	count := 0
+
+	load_balancer_interfaces.List(client, load_balancer_interfaces.ListOpts{}).EachPage(func(page pagination.Page) (bool, error) {
+		count++
+		actual, err := load_balancer_interfaces.ExtractLoadBalancerInterfaces(page)
+		if err != nil {
+			t.Errorf("Failed to extract Load Balancer Interfaces: %v", err)
+			return false, nil
+		}
+
+		th.CheckDeepEquals(t, ExpectedLoadBalancerInterfaceSlice, actual)
+
+		return true, nil
+	})
+
+	if count != 1 {
+		t.Errorf("Expected 1 page, got %d", count)
+	}
+}
+
+func TestGetLoadBalancerInterface(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/load_balancer_interfaces/5f3cae7c-58a5-4124-b622-9ca3cfbf2525", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, GetResponse)
+	})
+
+	s, err := load_balancer_interfaces.Get(fake.ServiceClient(), "5f3cae7c-58a5-4124-b622-9ca3cfbf2525").Extract()
+	th.AssertNoErr(t, err)
+	th.CheckDeepEquals(t, &LoadBalancerInterfaceDetail, s)
+}
+
+func TestUpdateLoadBalancerInterface(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/load_balancer_interfaces/ab49eb24-667f-4a4e-9421-b4d915bff416", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "PUT")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+		th.TestHeader(t, r, "Content-Type", "application/json")
+		th.TestHeader(t, r, "Accept", "application/json")
+		th.TestJSONRequest(t, r, UpdateRequest)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, UpdateResponse)
+	})
+
+	description := "test"
+	ipAddress := "100.64.64.34"
+	name := "Interface 1/2"
+	networkID := "e6106a35-d79b-44a3-bda0-6009b2f8775a"
+	virtualIPAddress := "100.64.64.101"
+	virtualIPProperties := load_balancer_interfaces.VirtualIPProperties{
+		Protocol: "vrrp",
+		Vrid:     10,
+	}
+
+	id := "2897f333-3554-4099-a638-64d7022bf9ae"
+	slotNumber := 2
+
+	status := "PENDING_UPDATE"
+
+	tenantID := "6a156ddf2ecd497ca786ff2da6df5aa8"
+
+	loadBalancerID := "9f872504-36ab-46af-83ce-a4991c669edd"
+
+	options := load_balancer_interfaces.UpdateOpts{
+		Description:         &description,
+		IPAddress:           ipAddress,
+		Name:                &name,
+		NetworkID:           "e6106a35-d79b-44a3-bda0-6009b2f8775a",
+		VirtualIPAddress:    "100.64.64.101",
+		VirtualIPProperties: virtualIPProperties,
+	}
+
+	s, err := load_balancer_interfaces.Update(fake.ServiceClient(), "ab49eb24-667f-4a4e-9421-b4d915bff416", options).Extract()
+	th.AssertNoErr(t, err)
+
+	th.CheckEquals(t, description, s.Description)
+	th.CheckEquals(t, id, s.ID)
+	th.CheckEquals(t, ipAddress, *s.IPAddress)
+	th.CheckEquals(t, loadBalancerID, s.LoadBalancerID)
+	th.CheckEquals(t, name, s.Name)
+	th.CheckEquals(t, networkID, *s.NetworkID)
+	th.CheckEquals(t, slotNumber, s.SlotNumber)
+	th.CheckEquals(t, status, s.Status)
+	th.CheckEquals(t, tenantID, s.TenantID)
+	th.CheckEquals(t, virtualIPAddress, *s.VirtualIPAddress)
+	th.CheckDeepEquals(t, virtualIPProperties, *s.VirtualIPProperties)
+}
+
+func TestIDFromName(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/load_balancer_interfaces", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, ListResponse)
+	})
+
+	client := fake.ServiceClient()
+
+	expectedID := "b409f68e-9307-4649-9073-bb3cb776bda5"
+	actualID, err := load_balancer_interfaces.IDFromName(client, "Interface 1/2")
+
+	th.AssertNoErr(t, err)
+	th.AssertEquals(t, expectedID, actualID)
+}
+
+func TestIDFromNameNoResult(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/load_balancer_interfaces", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, ListResponse)
+	})
+
+	client := fake.ServiceClient()
+
+	_, err := load_balancer_interfaces.IDFromName(client, "Interface X")
+
+	if err == nil {
+		t.Fatalf("Expected error, got none")
+	}
+
+}
+
+func TestIDFromNameDuplicated(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	th.Mux.HandleFunc("/v2.0/load_balancer_interfaces", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fake.TokenID)
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, ListResponseDuplicatedNames)
+	})
+
+	client := fake.ServiceClient()
+
+	_, err := load_balancer_interfaces.IDFromName(client, "Interface 1/2")
+
+	if err == nil {
+		t.Fatalf("Expected error, got none")
+	}
+}
---
+++ ecl/network/v2/load_balancer_plans/testing/fixtures.go
@@ -0,0 +1,132 @@
+package testing
+
+import (
+	load_balancer_plans "github.com/nttcom/eclcloud/ecl/network/v2/load_balancer_plans"
+)
+
+const ListResponse = `
+{
+  "load_balancer_plans": [
+    {
+      "description": "Load Balancer Description 1",
+      "enabled": true,
+      "id": "58ab4df4-10f2-4fa0-b374-74b06dd648ee",
+      "maximum_syslog_servers": 10,
+      "model": {
+        "edition": "Standard",
+        "size": "50"
+      },
+      "name": "LB_Plan1",
+      "vendor": "citrix",
+      "version": "10.5-57.7"
+    },
+    {
+      "description": "Load Balancer Description 2",
+      "enabled": false,
+      "id": "8b0cc5cc-b612-4810-ae45-7d6c5e806b3a",
+      "maximum_syslog_servers": 10,
+      "model": {
+        "edition": "Standard",
+        "size": "1000"
+      },
+      "name": "LB_Plan2",
+      "vendor": "citrix",
+      "version": "10.5-57.7"
+    }
+  ]
+}
+`
+const GetResponse = `
+{
+  "load_balancer_plan": {
+    "description": "Load Balance Plan Description",
+    "enabled": true,
+    "id": "6e5faf0c-9361-4b98-bfc4-670497c9bde3",
+    "maximum_syslog_servers": 10,
+    "model": {
+      "edition": "Standard",
+      "size": "50"
+    },
+    "name": "LB_Plan1",
+    "vendor": "citrix",
+    "version": "10.5-57.7"
+  }
+}
+  `
+
+var LoadBalancerPlan1 = load_balancer_plans.LoadBalancerPlan{
+	Description:          "Load Balancer Description 1",
+	Enabled:              true,
+	ID:                   "58ab4df4-10f2-4fa0-b374-74b06dd648ee",
+	MaximumSyslogServers: 10,
+	Model: load_balancer_plans.Model{
+		Edition: "Standard",
+		Size:    "50",
+	},
+	Name:    "LB_Plan1",
+	Vendor:  "citrix",
+	Version: "10.5-57.7",
+}
+
+var LoadBalancerPlan2 = load_balancer_plans.LoadBalancerPlan{
+	Description:          "Load Balancer Description 2",
+	Enabled:              false,
+	ID:                   "8b0cc5cc-b612-4810-ae45-7d6c5e806b3a",
+	MaximumSyslogServers: 10,
+	Model: load_balancer_plans.Model{
+		Edition: "Standard",
+		Size:    "1000",
+	},
+	Name:    "LB_Plan2",
+	Vendor:  "citrix",
+	Version: "10.5-57.7",
+}
+
+var LoadBalancerDetail = load_balancer_plans.LoadBalancerPlan{
+	Description:          "Load Balance Plan Description",
+	Enabled:              true,
+	ID:                   "6e5faf0c-9361-4b98-bfc4-670497c9bde3",
+	MaximumSyslogServers: 10,
+	Model: load_balancer_plans.Model{
+		Edition: "Standard",
+		Size:    "50",
+	},
+	Name:    "LB_Plan1",
+	Vendor:  "citrix",
+	Version: "10.5-57.7",
+}
+
+var ExpectedLoadBalancerPlanSlice = []load_balancer_plans.LoadBalancerPlan{LoadBalancerPlan1, LoadBalancerPlan2}
+
+const ListResponseDuplicatedNames = `
+{
+  "load_balancer_plans": [
+    {
+      "description": "Load Balancer Description 1",
+      "enabled": true,
+      "id": "58ab4df4-10f2-4fa0-b374-74b06dd648ee",
+      "maximum_syslog_servers": 10,
+      "model": {
+        "edition": "Standard",
+        "size": "50"
+      },
+      "name": "LB_Plan1",
+      "vendor": "citrix",
+      "version": "10.5-57.7"
+    },
+    {
+      "description": "Load Balancer Description 2",
+      "enabled": false,
+      "id": "8b0cc5cc-b612-4810-ae45-7d6c5e806b3a",
+      "maximum_syslog_servers": 10,
+      "model": {
+        "edition": "Standard",
+        "size": "1000"
+      },
+      "name": "LB_Plan1",
+      "vendor": "citrix",
+      "version": "10.5-57.7"
+    }
+  ]
+}
+`
