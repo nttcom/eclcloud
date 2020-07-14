33ed27cceb8d596c53e3f73967731d772804f46e
kazyshr <kazyshr0301@gmail.com>
2020-07-14 09:44:47 +0900

    Fix gateway interface

--- ecl/network/v2/gateway_interfaces/testing/fixtures.go
+++ ecl/network/v2/gateway_interfaces/testing/fixtures.go
@@ -11,7 +11,7 @@ const ListResponse = `
 			"aws_gw_id": null,
 			"azure_gw_id": null,
 			"description": "",
-			"fic_gw_id": "9c5c6441-e174-4b9a-9d16-4ed38cc95dd5",
+			"fic_gw_id": null,
 			"gcp_gw_id": null,
 			"gw_vipv4": "100.127.254.49",
 			"gw_vipv6": null,
@@ -34,14 +34,14 @@ const ListResponse = `
 		{
 			"aws_gw_id": null,
 			"azure_gw_id": null,
-			"description": "lab3-test-user-internet-gateway-interface, role : member",
+			"description": "lab3-test-user-fic-gateway-interface, role : member",
 			"fic_gw_id": "dd04adc4-459f-4fc4-83a5-47436c6aece5",
 			"gcp_gw_id": null,
 			"gw_vipv4": "100.127.254.1",
 			"gw_vipv6": null,
 			"id": "165ed64c-b9d4-46b1-afc1-cbbdc356ddcb",
 			"interdc_gw_id": null,
-			"internet_gw_id": "0ed09319-fe94-4618-8482-23ee254f3d2c",
+			"internet_gw_id": null,
 			"name": "lab3-hara-cfg-20151204",
 			"netmask": 29,
 			"network_id": "cce5c9a1-1ec3-40b1-bfc7-634bb914646b",
@@ -49,7 +49,7 @@ const ListResponse = `
 			"primary_ipv6": null,
 			"secondary_ipv4": "100.127.254.4",
 			"secondary_ipv6": null,
-			"service_type": "internet",
+			"service_type": "fic",
 			"status": "ACTIVE",
 			"tenant_id": "fe1f6fb95b0e48ba8c59be2121a58adc",
 			"vpn_gw_id": null,
@@ -64,7 +64,7 @@ const GetResponse = `
 		"aws_gw_id": null,
 		"azure_gw_id": null,
 		"description": "",
-		"fic_gw_id": "9c5c6441-e174-4b9a-9d16-4ed38cc95dd5",
+		"fic_gw_id": null,
 		"gcp_gw_id": null,
 		"gw_vipv4": "100.127.254.49",
 		"gw_vipv6": null,
@@ -90,7 +90,6 @@ const CreateRequest = `
 {
 	"gw_interface": {
 		"description": "",
-		"fic_gw_id": "9c5c6441-e174-4b9a-9d16-4ed38cc95dd5",
 		"gw_vipv4": "100.127.254.49",
 		"internet_gw_id": "e72ef35a-c96f-45f8-aeee-e7547c5b94b3",
 		"name": "5_Gateway",
@@ -99,7 +98,6 @@ const CreateRequest = `
 		"primary_ipv4": "100.127.254.53",
 		"secondary_ipv4": "100.127.254.54",
 		"service_type": "internet",
-		"tenant_id": "19ab165c7a664abe9c217334cd0e9cc9",
 		"vrid": 1
 	}
 }
@@ -108,19 +106,27 @@ const CreateRequest = `
 const CreateResponse = `
 {
 	"gw_interface": {
+		"aws_gw_id": null,
+		"azure_gw_id": null,
 		"description": "",
-		"fic_gw_id": "9c5c6441-e174-4b9a-9d16-4ed38cc95dd5",
+		"fic_gw_id": null,
+		"gcp_gw_id": null,
 		"gw_vipv4": "100.127.254.49",
+		"gw_vipv6": null,
 		"id": "09771fbb-6496-4ae1-9b53-226b6edcc1be",
+		"interdc_gw_id": null,
 		"internet_gw_id": "e72ef35a-c96f-45f8-aeee-e7547c5b94b3",
 		"name": "5_Gateway",
 		"netmask": 29,
 		"network_id": "0200a550-82cf-4d6d-b564-a87eb63e2b75",
 		"primary_ipv4": "100.127.254.53",
+		"primary_ipv6": null,
 		"secondary_ipv4": "100.127.254.54",
+		"secondary_ipv6": null,
 		"service_type": "internet",
 		"status": "PENDING_CREATE",
 		"tenant_id": "19ab165c7a664abe9c217334cd0e9cc9",
+		"vpn_gw_id": null,
 		"vrid": 1
 	}
 }`
@@ -129,33 +135,40 @@ const UpdateRequest = `
 {
 	"gw_interface": {
 		"description": "Updated",
-		"name": "5_Gateway"
+		"name": "6_Gateway"
 	}
 }`
 
 const UpdateResponse = `
 {
 	"gw_interface": {
+		"aws_gw_id": null,
+		"azure_gw_id": null,
 		"description": "Updated",
-		"fic_gw_id": "9c5c6441-e174-4b9a-9d16-4ed38cc95dd5",
+		"fic_gw_id": null,
+		"gcp_gw_id": null,
 		"gw_vipv4": "100.127.254.49",
+		"gw_vipv6": null,
 		"id": "09771fbb-6496-4ae1-9b53-226b6edcc1be",
+		"interdc_gw_id": null,
 		"internet_gw_id": "e72ef35a-c96f-45f8-aeee-e7547c5b94b3",
-		"name": "5_Gateway",
+		"name": "6_Gateway",
 		"netmask": 29,
 		"network_id": "0200a550-82cf-4d6d-b564-a87eb63e2b75",
 		"primary_ipv4": "100.127.254.53",
+		"primary_ipv6": null,
 		"secondary_ipv4": "100.127.254.54",
+		"secondary_ipv6": null,
 		"service_type": "internet",
 		"status": "PENDING_UPDATE",
 		"tenant_id": "19ab165c7a664abe9c217334cd0e9cc9",
+		"vpn_gw_id": null,
 		"vrid": 1
 	}
 }`
 
 var GatewayInterface1 = gateway_interfaces.GatewayInterface{
 	Description:   "",
-	FICGatewayID:  "9c5c6441-e174-4b9a-9d16-4ed38cc95dd5",
 	GwVipv4:       "100.127.254.49",
 	ID:            "09771fbb-6496-4ae1-9b53-226b6edcc1be",
 	InternetGwID:  "e72ef35a-c96f-45f8-aeee-e7547c5b94b3",
@@ -171,27 +184,18 @@ var GatewayInterface1 = gateway_interfaces.GatewayInterface{
 }
 
 var GatewayInterface2 = gateway_interfaces.GatewayInterface{
-	AwsGwID:       "",
-	AzureGwID:     "",
-	Description:   "lab3-test-user-internet-gateway-interface, role : member",
+	Description:   "lab3-test-user-fic-gateway-interface, role : member",
 	FICGatewayID:  "dd04adc4-459f-4fc4-83a5-47436c6aece5",
-	GcpGwID:       "",
 	GwVipv4:       "100.127.254.1",
-	GwVipv6:       "",
 	ID:            "165ed64c-b9d4-46b1-afc1-cbbdc356ddcb",
-	InterdcGwID:   "",
-	InternetGwID:  "0ed09319-fe94-4618-8482-23ee254f3d2c",
 	Name:          "lab3-hara-cfg-20151204",
 	Netmask:       29,
 	NetworkID:     "cce5c9a1-1ec3-40b1-bfc7-634bb914646b",
 	PrimaryIpv4:   "100.127.254.3",
-	PrimaryIpv6:   "",
 	SecondaryIpv4: "100.127.254.4",
-	SecondaryIpv6: "",
-	ServiceType:   "internet",
+	ServiceType:   "fic",
 	Status:        "ACTIVE",
 	TenantID:      "fe1f6fb95b0e48ba8c59be2121a58adc",
-	VpnGwID:       "",
 	VRID:          10,
 }
 
