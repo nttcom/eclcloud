ab3e9e6d9909725f32c6bdcd83cd9c557cfab87c
shimisho <shota.shimizu.nttcom@gmail.com>
2022-05-18 18:38:19 +0900

    modified two-byte space

--- v3/ecl/sss/v2/workspace_roles/testing/fixtures.go
+++ v3/ecl/sss/v2/workspace_roles/testing/fixtures.go
@@ -7,17 +7,17 @@ const workspaceID = "ws0000000001"
 const userID = "ecid1234567891"
 
 var createRequest = `
-{   
-　 "user_id": "ecid1234567891",
-   "workspace_id: "ws0000000001"
+{
+	"user_id": "ecid1234567891",
+	"workspace_id": "ws0000000001"
 }
 `
 
 var createResponse = `
 {
-    "user_id": "ecid1234567891",
-    "workspace_id": "ws0000000001",
-    "workspace_name": "testWorkspace001"
+	"user_id": "ecid1234567891",
+	"workspace_id": "ws0000000001",
+	"workspace_name": "testWorkspace001"
 }
 `
 
