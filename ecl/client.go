28617a575a4daeeb5f90815be2ce38da45cdb1a0
ikegami.shota <shota.ikegami@ntt.com>
2023-09-15 17:50:30 +0900

    IF-10358 Rollback under ecl directory

--- ecl/client.go
+++ ecl/client.go
@@ -348,19 +348,19 @@ func NewWorkflowV2(client *eclcloud.ProviderClient, eo eclcloud.EndpointOpts) (*
 	return initClientOpts(client, eo, "workflowv2")
 }
 
-// NewSecurityOrderV2 creates a ServiceClient that may be used to access the v2 Security
+// NewSecurityOrderV1 creates a ServiceClient that may be used to access the v1 Security
 // Order API service.
-func NewSecurityOrderV2(client *eclcloud.ProviderClient, eo eclcloud.EndpointOpts) (*eclcloud.ServiceClient, error) {
-	sc, err := initClientOpts(client, eo, "security-order")
-	// sc.ResourceBase = sc.Endpoint + "v3/"
+func NewSecurityOrderV1(client *eclcloud.ProviderClient, eo eclcloud.EndpointOpts) (*eclcloud.ServiceClient, error) {
+	sc, err := initClientOpts(client, eo, "mss-rfg")
+	// sc.ResourceBase = sc.Endpoint + "v2/"
 	return sc, err
 }
 
-// NewSecurityPortalV2 creates a ServiceClient that may be used to access the v2 Security
+// NewSecurityPortalV1 creates a ServiceClient that may be used to access the v1 Security
 // Portal API service.
-func NewSecurityPortalV2(client *eclcloud.ProviderClient, eo eclcloud.EndpointOpts) (*eclcloud.ServiceClient, error) {
-	sc, err := initClientOpts(client, eo, "security-operation")
-	// sc.ResourceBase = sc.Endpoint + "v3/"
+func NewSecurityPortalV1(client *eclcloud.ProviderClient, eo eclcloud.EndpointOpts) (*eclcloud.ServiceClient, error) {
+	sc, err := initClientOpts(client, eo, "mss-msa")
+	// sc.ResourceBase = sc.Endpoint + "v2/"
 	return sc, err
 }
 
